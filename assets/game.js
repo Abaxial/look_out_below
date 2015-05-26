491919c710ad4da7dd37fe29647dc0e689981a33
Shane Fera <shane.d.fera@gmail.com>
2015-05-26 12:23:55 -0400

    Commencing refactoring before moving on.

--- assets/game.js
+++ assets/game.js
@@ -1,15 +1,5 @@
 window.TP = {}
 
-Player = function(score_element, title_element) {
-  this.color = '#' + Math.floor(Math.random() * 16777215).toString(16);
-  this.score = 0
-  title_element.style.color = this.color
-  this.update_score = function() {
-    score_element.textContent = this.score
-  }
-}
-
-
 TP.init = function() {
   var self = this
   this.canvas = document.getElementById('canvas')
