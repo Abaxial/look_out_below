3b0ee3960cf61659205165577ae655e9ff79ae17
Shane Fera <shane.d.fera@gmail.com>
2016-04-09 10:20:01 -0400

    Framework integration: Konva

--- assets/game.js
+++ assets/game.js
@@ -1,114 +1,91 @@
-window.TP = {}
+window.Game = {}
 
-TP.init = function() {
-  var self = this
-  // Set the variables for game object.
-  this.canvas = document.getElementById('canvas')
-  this.ctx = canvas.getContext('2d')
-  this.width = canvas.width
-  this.height = canvas.height
+Game.init = function() {
+  var containerName = 'gameboard'
+  var width = document.getElementById(containerName).offsetWidth;
+  var height = width / 1.5
 
-  // Shapes on the canvas.
-  this.shapeList = []
+  var stage = new Konva.Stage({
+    container: 'gameboard',
+    width: width,
+    height: height
+  });
 
-  // Used to only initiate mousedown loop once.
-  this.mousedownID = -1
+  var tween = null;
 
-  // Initialize 2 players.
-  var player1 = new Player(document.getElementById('p1score'), document.getElementById('p1name'))
-  var player2 = new Player(document.getElementById('p2score'), document.getElementById('p2name'))
-  player1.update_score()
-  player2.update_score()
+  function addStar(layer, stage) {
+    var scale = Math.random();
 
-  //
-  this.players = [player1, player2]
-  this.activePlayer = 0
+    var star = new Konva.Star({
+      x: Math.random() * stage.getWidth(),
+      y: Math.random() * stage.getHeight(),
+      numPoints: 5,
+      innerRadius: 30,
+      outerRadius: 50,
+      fill: '#89b717',
+      opacity: 0.8,
+      draggable: true,
+      scale: {
+        x : scale,
+        y : scale
+      },
+      rotation: Math.random() * 180,
+      shadowColor: 'black',
+      shadowBlur: 10,
+      shadowOffset: {
+        x : 5,
+        y : 5
+      },
+      shadowOpacity: 0.6,
+      // custom attribute
+      startScale: scale
+    });
 
-  canvas.style.cursor = "pointer";
-
-  canvas.onmousedown = function(e){
-
-    size = 8
-
-    if (clickedShape(e.offsetX, e.offsetY, self.shapeList).isConflict == true) {
-
-    } else if (TP.mousedownID == -1) {  //Prevent multiple loops!
-      TP.mousedownID = setInterval(whilemousedown, 2 /*execute every 100ms*/)
-    }
-
-    var growing = true
-
-    tempSquare = new Square(e.offsetX, e.offsetY, size, self.ctx)
-    function whilemousedown() {
-      canvas.onmousemove = function(e) {
-        if (TP.mousedownID != -1) {
-          tempSquare.x = e.offsetX
-          tempSquare.y = e.offsetY
-          TP.render()
-          tempSquare.render()
-        }
-      }
-      if (size < 200 && growing == true) {
-        size = size + 2
-      } else if (size == 200 && growing == true) {
-        growing = false
-        TP.render()
-      } else if (size > 8 && growing == false) {
-        size = size - 2
-        TP.render()
-      } else if (size == 8  && growing == false) {
-        growing = true
-      }
-
-      tempSquare.size = size;
-      tempSquare.color = "#cccccc"
-      tempSquare.render()
-    }
+    layer.add(star);
   }
 
-  canvas.onmouseup = function(e) {
-    TP.render()
 
-    if(TP.mousedownID != -1) {
-      clearInterval(TP.mousedownID);
-      TP.mousedownID = -1
+  var layer = new Konva.Layer();
+  var dragLayer = new Konva.Layer();
+
+  for(var n = 0; n < 30; n++) {
+    addStar(layer, stage);
+  }
+
+  stage.add(layer, dragLayer);
+
+  stage.on('dragstart', function(evt) {
+    var shape = evt.target;
+    // moving to another layer will improve dragging performance
+    shape.moveTo(dragLayer);
+    stage.draw();
+
+    if (tween) {
+      tween.pause();
     }
-
-    var square = new Square(e.offsetX, e.offsetY, size, self.ctx)
-
-    while (square.size > 10) {
-      if (square.conflict(self.shapeList)) {
-        square.size = square.size - Math.floor(Math.random() * square.size)
-      } else {
-        break;
+    shape.setAttrs({
+      shadowOffset: {
+        x: 15,
+        y: 15
+      },
+      scale: {
+        x: shape.getAttr('startScale') * 1.2,
+        y: shape.getAttr('startScale') * 1.2
       }
-    }
+    });
+  });
 
-    if (square.size < 10) {
-      square.size = 10
-    }
-
-    if (!square.conflict(self.shapeList)) {
-      square.color = TP.players[TP.activePlayer].color
-      square.render()
-      self.shapeList.push(square)
-      TP.players[TP.activePlayer].score += (square.size * square.size)
-      TP.players[TP.activePlayer].update_score()
-      TP.activePlayer = 1 - TP.activePlayer
-    } else if (square.wasClicked(e.offsetX, e.offsetY, self.shapeList)) {
-      removeClickedShape(e.offsetX, e.offsetY, self.shapeList)
-      TP.activePlayer = 1 - TP.activePlayer
-    } else {
-      alert("Please try again, attempt too close to other shape.")
-    }
-
-    TP.render()
-  }
-}
-
-TP.render = function() {
-  this.ctx.clearRect(0,0,this.width,this.height)
-  for(index in this.shapeList) {
-    this.shapeList[index].render()
-  }
+  stage.on('dragend', function(evt) {
+    var shape = evt.target;
+    shape.moveTo(layer);
+    stage.draw();
+    shape.to({
+      duration: 0.5,
+      easing: Konva.Easings.ElasticEaseOut,
+      scaleX: shape.getAttr('startScale'),
+      scaleY: shape.getAttr('startScale'),
+      shadowOffsetX: 5,
+      shadowOffsetY: 5
+    });
+  });
 }
